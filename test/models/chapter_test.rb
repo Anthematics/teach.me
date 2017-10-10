0b6d6eb657a3df4ddefe5f9b774684c9f6e0a1ab
KaiserChan <chankaiserkk@gmail.com>
2017-10-10 17:30:17 -0400

    updated teachme ... created seed data for user, chapter and steps ... added test for chapter and steps, all passed

--- test/models/chapter_test.rb
+++ test/models/chapter_test.rb
@@ -4,4 +4,15 @@ class ChapterTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_creation_of_chapter
+    #code
+    chapter = Chapter.new(
+      name: "some chapter",
+      description: "some description",
+    )
+    chapter.save
+    assert chapter.persisted?
+
+  end
 end
--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -4,4 +4,17 @@ class StepTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_creation_of_step
+    chapter = Chapter.new(
+      name: "some chapter",
+      description: "some description",
+    )
+    step = Step.new(
+      name: "Create new variables",
+      description: "Try create some new variables yourself",
+      chapter: chapter
+    )
+    step.save
+    assert step.persisted?
+  end
 end
