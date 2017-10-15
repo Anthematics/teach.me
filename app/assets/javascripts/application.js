36f88118fe5213063debcae670ba9d7c5c6d3696
KaiserChan <chankaiserkk@gmail.com>
2017-10-15 15:08:00 -0400

    updated teachme ... fixed footer style and position ... fixed languages card display bug, now the hovering effect works (still need to tighten the codes)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,8 +21,58 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-  let signUp = document.querySelector('#signup');
-  let logIn = document.querySelector('#login');
+// On languages page, Ruby card styling below ------
+  let rubyText = document.querySelector('#ruby-text');
+  let rubyImage = document.querySelector('#ruby-image');
+  let rubyCard = document.querySelector('#ruby-card');
 
+  rubyCard.addEventListener('mouseover',rubyMouseoverCardDisplay);
+  rubyCard.addEventListener('mouseout', rubyMouseoutCardDisplay);
+
+  function rubyMouseoverCardDisplay() {
+    rubyImage.style.opacity = "0.2";
+    rubyText.style.opacity = "1";
+  }
+
+  function rubyMouseoutCardDisplay() {
+    rubyImage.style.opacity = "1";
+    rubyText.style.opacity = "0";
+  }
+
+// On languages page, JS card styling below ------
+  let jsText = document.querySelector('#js-text');
+  let jsImage = document.querySelector('#js-image');
+  let jsCard = document.querySelector('#js-card');
+
+  jsCard.addEventListener('mouseover', jsMouseoverCardDisplay);
+  jsCard.addEventListener('mouseout', jsMouseoutCardDisplay);
+
+  function jsMouseoverCardDisplay() {
+    jsImage.style.opacity = "0.2";
+    jsText.style.opacity = "1";
+  }
+
+  function jsMouseoutCardDisplay() {
+    jsImage.style.opacity = "1";
+    jsText.style.opacity = "0";
+  }
+
+// On languages page, Pyhon card styling below ------
+  let pythonText = document.querySelector('#python-text');
+  let pythonImage = document.querySelector('#python-image');
+  let pythonCard = document.querySelector('#python-card');
+
+  pythonCard.addEventListener('mouseover', pythonMouseoverCardDisplay);
+  pythonCard.addEventListener('mouseout', pythonMouseoutCardDisplay);
+
+  function pythonMouseoverCardDisplay() {
+    pythonImage.style.opacity = "0.1";
+    pythonText.style.opacity = "1";
+  }
+
+  function pythonMouseoutCardDisplay() {
+    pythonImage.style.opacity = "1";
+    pythonText.style.opacity = "0";
+  }
 
 });
