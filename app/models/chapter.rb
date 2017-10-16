4a293d387d2f437578beb798fa50a78277db47a7
tenzin2017 <tenzinleo@gmail.com>
2017-10-16 11:46:51 -0400

    added has_many:users to step and chapter model

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,3 +1,4 @@
 class Chapter < ApplicationRecord
-	has_many :steps
+  has_many :steps
+  has_many :users
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,4 @@
 class Step < ApplicationRecord
   belongs_to :chapter
+  has_many :users
 end
