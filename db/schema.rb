56c79d0eddffdb69f1d951c01c19a484c5a15582
tenzin2017 <tenzinleo@gmail.com>
2017-10-16 11:07:46 -0400

    added user_id column to steps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016144802) do
+ActiveRecord::Schema.define(version: 20171016150459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171016144802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "code"
+    t.integer "user_id"
     t.index ["chapter_id"], name: "index_steps_on_chapter_id"
   end
 
