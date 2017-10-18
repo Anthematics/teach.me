d8ff17a50419d6f1077632b9a060f35c1f07c7f4
KaiserChan <chankaiserkk@gmail.com>
2017-10-18 13:47:01 -0400

    updated teachme ... swapped js of menu.js and languages.js ... started fixing accordion at languages index.html.erb ... using html/css and js for that

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -1,94 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
 
-// // On languages page, Ruby card styling below ------
-//   var rubyText = document.querySelector('#ruby-text');
-//   var rubyImage = document.querySelector('#ruby-image');
-//   var rubyCard = document.querySelector('#ruby-card');
-//
-//   rubyCard.addEventListener('mouseover',rubyMouseoverCardDisplay);
-//   rubyCard.addEventListener('mouseout', rubyMouseoutCardDisplay);
-//
-//   function rubyMouseoverCardDisplay() {
-//     rubyImage.style.opacity = "0.2";
-//     rubyText.style.opacity = "1";
-//   }
-//
-//   function rubyMouseoutCardDisplay() {
-//     rubyImage.style.opacity = "1";
-//     rubyText.style.opacity = "0";
-//   }
-//
-// // On languages page, JS card styling below ------
-//   var jsText = document.querySelector('#js-text');
-//   var jsImage = document.querySelector('#js-image');
-//   var jsCard = document.querySelector('#js-card');
-//
-//   jsCard.addEventListener('mouseover', jsMouseoverCardDisplay);
-//   jsCard.addEventListener('mouseout', jsMouseoutCardDisplay);
-//
-//   function jsMouseoverCardDisplay() {
-//     jsImage.style.opacity = "0.2";
-//     jsText.style.opacity = "1";
-//   }
-//
-//   function jsMouseoutCardDisplay() {
-//     jsImage.style.opacity = "1";
-//     jsText.style.opacity = "0";
-//   }
-//
-// // On languages page, Pyhon card styling below ------
-//   var pythonText = document.querySelector('#python-text');
-//   var pythonImage = document.querySelector('#python-image');
-//   var pythonCard = document.querySelector('#python-card');
-//
-//   pythonCard.addEventListener('mouseover', pythonMouseoverCardDisplay);
-//   pythonCard.addEventListener('mouseout', pythonMouseoutCardDisplay);
-//
-//   function pythonMouseoverCardDisplay() {
-//     pythonImage.style.opacity = "0.1";
-//     pythonText.style.opacity = "1";
-//   }
-//
-//   function pythonMouseoutCardDisplay() {
-//     pythonImage.style.opacity = "1";
-//     pythonText.style.opacity = "0";
-//   }
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// On languages page, language cards styling below ------
-  var cardBody = document.querySelectorAll('.card-body');
-  var cardImage = document.querySelectorAll('.card-image');
-  var languageCard = document.querySelectorAll('.language-card');
-
-  for (var i = 0; i < languageCard.length; i++) {
-    languageCard.addEventListener('mouseover', mouseoverCardDisplay);
-    languageCard.addEventListener('mouseout', mouseoutCardDisplay);
-
-    function mouseoverCardDisplay() {
-      cardImage[i].style.opacity = "0.1";
-      cardBody[i].style.opacity = "1";
-    }
-
-    function mouseoutCardDisplay() {
-      cardImage[i].style.opacity = "1";
-      cardBody[i].style.opacity = "0";
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   }
-
-  // var languageCard = document.querySelector('#cards');
-  //
-  // languageCard.addEventListener('mouseover',function(e) {
-  //   var languageCard = e.target.parentNode
-  //   if (languageCard.className === 'language-card') {
-  //     languageCard.querySelector('.card-body').style.opacity = '1';
-  //     languageCard.querySelector('.card-image').style.opacity = '0.1';
-  //   }
-  // });
-  //
-  // languageCard.addEventListener('mouseout',function(e) {
-  //   var languageCard = e.target.parentNode
-  //   if (languageCard.className === 'language-card') {
-  //     languageCard.querySelector('.card-body').style.opacity = '0';
-  //     languageCard.querySelector('.card-image').style.opacity = '1';
-  //   }
-  // });
-});
+}
+</script>
