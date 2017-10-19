9ea57a5ffbff8e7784e7a3f7aeaeb440bf3682c0
Anthematics <jasonharder2@gmail.com>
2017-10-19 16:38:07 -0400

    fixed steps route test

--- test/controllers/steps_controller_test.rb
+++ test/controllers/steps_controller_test.rb
@@ -1,24 +1,29 @@
 require 'test_helper'
 
 class StepsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get show" do
-  #   get steps_show_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get new" do
-  #   get steps_new_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get create" do
-  #   get steps_create_url
-  #   assert_response :success
-  # end
-  #
-  # test "should get edit" do
-  #   get steps_edit_url
-  #   assert_response :success
-  # end
+
+
+test "should get show" do
+		get '/languages/ruby/chapters/1/steps/1'
+		assert_response :success
+	end
+
+	test "should get new" do
+		skip
+		get steps_new_url
+		assert_response :success
+	end
+
+	test "should get create" do
+		skip
+		get steps_create_url
+		assert_response :success
+	end
+
+	test "should get edit" do
+		skip
+		get steps_edit_url
+		assert_response :success
+	end
 
 end
