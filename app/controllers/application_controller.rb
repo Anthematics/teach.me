dabd1424b11232b4b42bed774d8280d73d4c3af9
KaiserChan <chankaiserkk@gmail.com>
2017-10-19 11:10:52 -0400

    updated teachme ... consolidated scripts at <head> ... removed the accidental extra <head> inside <head> ... removed require turbolink so that /menu now works properly again ... fixed footer positioning ... fixed bug where signed-in information wasn't showing at home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,22 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
-
-
-private
-
-	def current_user
-		@current_user ||= User.find(session[:user_id]) if session[:user_id]
-	end
+  protect_from_forgery with: :exception
 
 helper_method :current_user
-	def ensure_logged_in
-		unless current_user
-			flash[:alert] = "Please Log In"
-			redirect_to root_path
-		end
-	end
+private
+
+  def current_user
+
+    @loggedin_user ||= User.find(session[:user_id]) if session[:user_id]
+    #puts @current_user.email
+  end
+
+
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please Log In"
+      redirect_to root_path
+    end
+  end
 
 end
