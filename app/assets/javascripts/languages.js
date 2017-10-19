79f59b8ad253b5455f3cd0e852b05f8a50608b24
KaiserChan <chankaiserkk@gmail.com>
2017-10-18 22:17:28 -0400

    updated teachme ... added logo, fixed sizing and positioning of logo, fixed positioning of post-login info, replaced broken python logo ... STILL working on layer 2 accordion

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -1,6 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
 document.addEventListener("DOMContentLoaded", function() {
 
   var acc = document.getElementsByClassName("accordion");
@@ -17,4 +16,5 @@ document.addEventListener("DOMContentLoaded", function() {
       };
     };
   };
-})
+
+});
