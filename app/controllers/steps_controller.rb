4a7e1471cf90fd96b92eb37e3a5673124c5337ae
tenzin2017 <tenzinleo@gmail.com>
2017-10-24 13:02:07 -0400

    cleaned the code and wrote comments where required

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -11,17 +11,18 @@ class StepsController < ApplicationController
   def show
     @step = Step.find(params[:id])
 
+    #array of steps of current chapter of current language
     @steps = current_language.chapters.find(params[:chapter_id]).steps.all
+    #number of steps finished by current user
     @user_steps = current_user.user_steps.count
-   puts @user_steps
+    # total steps are total steps of current chapter
     @total_steps = @steps.count
-   puts @total_steps
+
     @progress = ((@user_steps.to_f/@total_steps.to_f) * 100).to_i
 
   end
 
   def create
-
   end
 
   def current_language
