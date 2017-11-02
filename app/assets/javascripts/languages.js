efc554f6da77c290ec4d4025e5941d10670a64c8
Navi Mann <navdeep.k.mann@gmail.com>
2017-11-01 23:22:29 -0400

    Reverted javascript and python changes and added COMING SOON messages

--- app/assets/javascripts/languages.js
+++ app/assets/javascripts/languages.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
       rubyCard.addEventListener('mouseout', rubyMouseoutCardDisplay);
     }
 
+
     function rubyMouseoverCardDisplay() {
       rubyImage.style.opacity = "0.2";
       rubyText.style.opacity = "1";
@@ -39,4 +40,64 @@ document.addEventListener("DOMContentLoaded", function() {
       rubyImage.style.opacity = "1";
       rubyText.style.opacity = "0";
     }
+
+  // On languages page, JS card styling below ------
+    var jsText = document.querySelector('#js-text');
+    var jsImage = document.querySelector('#js-image');
+    var jsCard = document.querySelector('#js-card');
+
+    if (jsCard) {
+      jsCard.addEventListener('mouseover', jsMouseoverCardDisplay);
+      jsCard.addEventListener('mouseout', jsMouseoutCardDisplay);
+    }
+
+    function jsMouseoverCardDisplay() {
+      jsImage.style.opacity = "0.2";
+      jsText.style.opacity = "1";
+    }
+
+    function jsMouseoutCardDisplay() {
+      jsImage.style.opacity = "1";
+      jsText.style.opacity = "0";
+    }
+
+  // On languages page, Pyhon card styling below ------
+    var pythonText = document.querySelector('#python-text');
+    var pythonImage = document.querySelector('#python-image');
+    var pythonCard = document.querySelector('#python-card');
+
+    if(pythonCard) {
+      pythonCard.addEventListener('mouseover', pythonMouseoverCardDisplay);
+      pythonCard.addEventListener('mouseout', pythonMouseoutCardDisplay);
+    }
+
+    function pythonMouseoverCardDisplay() {
+      pythonImage.style.opacity = "0.1";
+      pythonText.style.opacity = "1";
+    }
+
+    function pythonMouseoutCardDisplay() {
+      pythonImage.style.opacity = "1";
+      pythonText.style.opacity = "0";
+    }
+
+  // On languages page, language cards styling below ------
+    // var cardBody = document.querySelector('.card-body');
+    // var cardImage = document.querySelector('.card-image');
+    // var languageCard = document.querySelector('.language-card');
+    //
+    // languageCard.addEventListener('mouseover', mouseoverCardDisplay);
+    // languageCard.addEventListener('mouseout', mouseoutCardDisplay);
+    //
+    // function mouseoverCardDisplay() {
+    //   cardImage.style.opacity = "0.1";
+    //   cardBody.style.opacity = "1";
+    // }
+    //
+    // function mouseoutCardDisplay() {
+    //   cardImage.style.opacity = "1";
+    //   cardBody.style.opacity = "0";
+    // }
+
+    
 });
